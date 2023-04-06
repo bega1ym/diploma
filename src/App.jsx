d7dfc3b228b7091bdcff6f49744fe6561442fdff
Seksenova Begaiym <begaym01@gmail.com>
2023-04-06 15:08:11 +0600

    added two nav links with h2 tag

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ export default function App() {
         <Routes>
           <Route path="/" element={<h2>Home</h2>} exact />
           <Route path="/about" element={<h2>About</h2>} exact />
+          <Route path="/contacts" element={<h2>Contacts</h2>} exact />
+          <Route path="/delivery" element={<h2>Delivery</h2>} exact />
         </Routes>
       </Layout>
     </div>
