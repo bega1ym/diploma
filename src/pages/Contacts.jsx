8b2fe39969c53d006a7fe439535ef4255a8afca6
Seksenova Begaiym <begaym01@gmail.com>
2023-04-11 16:08:48 +0600

    added contact information

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,10 +1,39 @@
 export default function Contacts() {
   return (
     <div className="Contacts">
-      <h1> The first offical authorized partner of Apple in Kyrgyzstan with the status of Apple Authorized Reseller</h1>
-      <p> In our store you can not only buy an iPhone, Mac computer or iPad tablet, but also other Apple equipment and accessories for it.  Asiastore is a place where you will be helped with the choice of equipment, taught how to use it, give advice on the operation of your gadgets and offer high-quality accessories.</p>
-      
-      <p>Our store is the only authorized reseller of Apple and Garmin in Kyrgyzstan, as well as the official distributor of JBL & Harman Kardon and smart devices from Yandex</p>
+      <h1>
+        {" "}
+        The first offical authorized partner of Apple in Kyrgyzstan with the
+        status of Apple Authorized Reseller
+      </h1>
+
+      <h2>Schedule:</h2>
+
+      <p>Monday - Friday: 09:00 - 19:00 (without lunch) <br></br>
+Saturday: 09:00 - 18:00 (without lunch)<br></br>
+Sunday: 10:00 - 17:00 (no lunch, delivery only, no pickup)<br></br>
+
+Wednesday: 15:00 - 16:30 - technical break</p>
+      <p>
+
+        <h2>Buyers and Clients:</h2>
+
+
+        <p>0(3922)-7-25-40</p>
+        <p>+(996)702-45-36-80</p>
+        {" "}
+        In our store you can not only buy an iPhone, Mac computer or iPad
+        tablet, but also other Apple equipment and accessories for it. Asiastore
+        is a place where you will be helped with the choice of equipment, taught
+        how to use it, give advice on the operation of your gadgets and offer
+        high-quality accessories.
+      </p>
+
+      <p>
+        Our store is the only authorized reseller of Apple and Garmin in
+        Kyrgyzstan, as well as the official distributor of JBL & Harman Kardon
+        and smart devices from Yandex
+      </p>
     </div>
   );
 }
