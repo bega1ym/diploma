411ceee785658a5f8dcbfe40d199aa5cebb65150
Seksenova Begaiym <begaym01@gmail.com>
2023-04-11 16:18:44 +0600

    added more information : p,h2, ul, ,li with texts insait file about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,18 @@ export default function About() {
   return (
     <div className="About">
       <h1> The first offical authorized partner of Apple in Kyrgyzstan with the status of Apple Authorized Reseller</h1>
+
+      <p>Friends, are you planning to make a purchase with maximum comfort, minimum risks and a favorable price? You were in the right place, at the right time. Welcome to us - to the world of the online store.</p>
+
+      <p>With us you will definitely find what you are looking for, and we will do everything to make you have a pleasant experience with our pricing policy and service. We know how important it is not to waste time searching and waiting, so our service is a wide range of products combined with punctual and free delivery (upon a certain amount). We know that we will not get a second chance to make a first impression, and we value the trust of our clients the most.</p>
+
+      <h2>Now it will become even more convenient to make purchases via the Internet, because we have collected in one store:</h2>
+      <ul>
+        <li>iPhone</li>
+        <li>Samsung</li>
+        <li>Xiaomi</li>
+        <li>phone accessories</li>
+      </ul>
       <p> In our store you can not only buy an iPhone, Mac computer or iPad tablet, but also other Apple equipment and accessories for it.  Asiastore is a place where you will be helped with the choice of equipment, taught how to use it, give advice on the operation of your gadgets and offer high-quality accessories.</p>
       
       <p>Our store is the only authorized reseller of Apple and Garmin in Kyrgyzstan, as well as the official distributor of JBL & Harman Kardon and smart devices from Yandex</p>
