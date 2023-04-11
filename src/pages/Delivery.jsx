f1d91e93f8e31504145ef2b6b2801d27c5ea1cde
Seksenova Begaiym <begaym01@gmail.com>
2023-04-11 15:37:14 +0600

    added logo.svg

---
+++ src/pages/Delivery.jsx
@@ -0,0 +1,45 @@
+export default function Delivery() {
+  return (
+    <div className="Delivery">
+      <h1>
+        {" "}
+        The first offical authorized partner of Apple in Kyrgyzstan with the
+        status of Apple Authorized Reseller
+      </h1>
+      <h2>Delivery and pickup of goods in Bishkek, Kyrgyzstan</h2>
+
+      <br></br>
+
+      <p>
+        Delivery by courier in Bishkek Cost of services: Delivery of orders
+        worth more than 2500 soms, in Bishkek* - free of charge (within 10 km
+        from the office) Delivery of orders worth up to 2500 soms, in Bishkek -
+        199 soms
+      </p>
+
+      <h2>Delivery in Kyrgyzstan by courier service</h2>
+
+      <p>
+        Delivery of goods is carried out by a third-party courier service to
+        most settlements in Kyrgyzstan after full prepayment for the order and
+        delivery Delivery time from 1 to 2 working days, depending on the region
+        of the recipient. Delivery cost from 200 soms.
+      </p>
+
+      <p>
+        {" "}
+        In our store you can not only buy an iPhone, Mac computer or iPad
+        tablet, but also other Apple equipment and accessories for it. Asiastore
+        is a place where you will be helped with the choice of equipment, taught
+        how to use it, give advice on the operation of your gadgets and offer
+        high-quality accessories.
+      </p>
+
+      <p>
+        Our store is the only authorized reseller of Apple and Garmin in
+        Kyrgyzstan, as well as the official distributor of JBL & Harman Kardon
+        and smart devices from Yandex
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="Home">
+      <h1> Welcome to my Phone shop</h1>
+      <p> here you will find best devices for you and your family</p>
+      <p>We have only original equipment at reasonable prices</p>
+    </div>
+  );
+}
