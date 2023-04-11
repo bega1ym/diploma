d71c16f6824a90acd7f5127ebe2e9a53e88eccf7
Seksenova Begaiym <begaym01@gmail.com>
2023-04-11 16:17:25 +0600

    added useEfect , snapshot , get docs < category collection and import

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./CategoryList.css";
 import { NavLink } from "react-router-dom";
+import {  getDocs, onSnapshot } from "firebase/firestore";
+import { categoryCollection } from "../../firebase";
 
 export default function CategoryList() {
   const [categories, setCategories] = useState([]);
+
+  useEffect (()=> {
+    getDocs (categoryCollection)
+    .then (snapshot => {
+      const newCategories =[];
+
+      snapshot.docs.forEach (doc => {
+        const category = doc.data ();
+        category.id = doc.id;
+
+        newCategories.push(category);
+      });
+      setCategories(newCategories);
+    })
+  }, [])
 
   const output = categories.map((category) => (
     <li key={category.id}>
