63b5487b3917b2db95f58d7af848ee52b8ecfa25
Seksenova Begaiym <begaym01@gmail.com>
2023-04-11 16:20:46 +0600

    formatted the document

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -9,24 +9,21 @@ export default function Contacts() {
 
       <h2>Schedule:</h2>
 
-      <p>Monday - Friday: 09:00 - 19:00 (without lunch) <br></br>
-Saturday: 09:00 - 18:00 (without lunch)<br></br>
-Sunday: 10:00 - 17:00 (no lunch, delivery only, no pickup)<br></br>
-
-Wednesday: 15:00 - 16:30 - technical break</p>
       <p>
+        Monday - Friday: 09:00 - 19:00 (without lunch) <br></br>
+        Saturday: 09:00 - 18:00 (without lunch)<br></br>
+        Sunday: 10:00 - 17:00 (no lunch, delivery only, no pickup)<br></br>
+        Wednesday: 15:00 - 16:30 - technical break
+      </p>
 
+      <p>
         <h2>Buyers and Clients:</h2>
-
-
         <p>0(3922)-7-25-40</p>
-        <p>+(996)702-45-36-80</p>
-        {" "}
-        In our store you can not only buy an iPhone, Mac computer or iPad
-        tablet, but also other Apple equipment and accessories for it. Asiastore
-        is a place where you will be helped with the choice of equipment, taught
-        how to use it, give advice on the operation of your gadgets and offer
-        high-quality accessories.
+        <p>+(996)702-45-36-80</p> In our store you can not only buy an iPhone,
+        Mac computer or iPad tablet, but also other Apple equipment and
+        accessories for it. Asiastore is a place where you will be helped with
+        the choice of equipment, taught how to use it, give advice on the
+        operation of your gadgets and offer high-quality accessories.
       </p>
 
       <p>
