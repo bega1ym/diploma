b20992f19e7572c697e755ed5abdcdc92c15c6af
Seksenova Begaiym <begaym01@gmail.com>
2023-05-19 13:25:25 +0600

    filling the site about delivery

---
+++ src/pages/Delivery.jsx
@@ -0,0 +1,26 @@
+import "./Delivery.css"
+export default function Delivery() {
+  return (
+    <div className="Delivery">
+      <h1>- Delivery in Bishkek Paid</h1>
+
+      
+      <br></br>
+      <p>SUNDAY DELIVERY
+NOT IMPLEMENTED
+<br></br>
+
+
+RECEIVING ORDERS FROM 10-00 TO 15-00
+<br></br>
+
+GOODS DELIVERY IS CARRIED OUT FROM 12-00 TO 18-00</p>
+
+<h2>Contact phone numbers for orders:</h2>
+<p>+996 700 916121
+  <br></br>
++996 551 916122</p>
+  
+    </div>
+  );
+}
