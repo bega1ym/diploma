f937d5d4d3e587ddc6712254d39fca2a768038e2
Seksenova Begaiym <begaym01@gmail.com>
2023-05-19 11:26:13 +0600

    filled the folder

---
+++ src/pages/About.jsx
@@ -0,0 +1,24 @@
+import "./About.css"
+
+export default function About() {
+  return (
+    <div className="About">
+      <h2>About Us</h2>
+      <p>
+        We are an online store that specializes in selling smartphones and
+        accessories. Here you can find everything you need to accessorize your
+        phone, including phone cases, headphones, power banks, and more.
+      </p>
+      <p>
+        Our mission is to provide our customers with high-quality products at
+        affordable prices. We believe that everyone should have access to the
+        latest technology without breaking the bank.
+      </p>
+      <p>
+        If you have any questions or concerns, please feel free to reach out to
+        us. Our customer support team is available 24/7 to assist you with your
+        needs.
+      </p>
+    </div>
+  );
+}
