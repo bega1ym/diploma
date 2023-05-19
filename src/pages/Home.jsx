367ddfae583b010fcf487f6ecc52930d183fd361
Seksenova Begaiym <begaym01@gmail.com>
2023-05-19 13:20:08 +0600

    changed color

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import "./Home.css"
 export default function Home() {
   return (
     <div className="Home">
       <h1>Welcome to my Phone shop</h1>
+
+      
+      <br></br>
+      <p>Looking for Xiaomi, Nobby, Lenovo, Meizu phones at the most affordable price in Bishkek? In the store "Gadget" you will find a wide selection of phones, tablets. We deliver in Bishkek and Kyrgyzstan. The store manager will be happy to advise you. Write to us on WhatsApp or Chat!</p>
   
     </div>
   );
