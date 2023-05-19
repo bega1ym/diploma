0b923a0c44e98dfd6c1a05b684bb267c808e4aee
Seksenova Begaiym <begaym01@gmail.com>
2023-05-19 11:08:01 +0600

    wrote in the dom folder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
     <div className="Home">
-      <h1>Welcome to my Ice-Cream shop</h1>
-      <p>This is my awesome Ice-Cream shop.</p>
-      <p>We only use natural ingredients to make our yummy ice-creams</p>
+      <h1>Welcome to my Phone shop</h1>
+  
     </div>
   );
 }
