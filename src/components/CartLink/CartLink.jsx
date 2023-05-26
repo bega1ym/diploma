ed81bf5159d1bfef5cb4f4f741cd2527c854c637
Seksenova Begaiym <begaym01@gmail.com>
2023-05-26 15:15:35 +0600

    formatted the document

--- src/components/CartLink/CartLink.jsx
+++ src/components/CartLink/CartLink.jsx
@@ -3,18 +3,21 @@ import { AppContext } from "../../App";
 import { NavLink } from "react-router-dom";
 import { useContext } from "react";
 
-export default function CartLink(){
-  const {cart} = useContext(AppContext);
+export default function CartLink() {
+  const { cart } = useContext(AppContext);
 
   //вычислить кол-во товаров
-  const total = Object.values(cart).reduce(
-    (acc, num) => acc + num, 0
-  );
+  const total = Object.values(cart).reduce((acc, num) => acc + num, 0);
 
-  return(
+  return (
     <div className="CartLink">
       <NavLink to="/cart">
-        <img className="cart" src="https://www.shareicon.net/data/512x512/2016/02/07/281223_cart_512x512.png" alt="cart" />({total})
+        <img
+          className="cart"
+          src="https://www.shareicon.net/data/512x512/2016/02/07/281223_cart_512x512.png"
+          alt="cart"
+        />
+        ({total})
       </NavLink>
     </div>
   );
