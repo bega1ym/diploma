7785163b92a017a04a24411044f6d4f6e22602c4
Seksenova Begaiym <begaym01@gmail.com>
2023-05-26 15:13:33 +0600

    formatted the document

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -3,7 +3,6 @@ import { AppContext } from "../../App";
 import { productCollection, uploadProductPhoto } from "../../firebase";
 import { addDoc } from "firebase/firestore";
 
-
 export default function AddProduct({ category }) {
   const { user } = useContext(AppContext);
   const [name, setName] = useState("");
