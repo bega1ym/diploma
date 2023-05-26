a1a8f4865087f049d1502937efb2d4023a53c4da
Seksenova Begaiym <begaym01@gmail.com>
2023-05-26 15:21:23 +0600

    formatted the document

--- src/components/OrderList/OrderList.jsx
+++ src/components/OrderList/OrderList.jsx
@@ -9,33 +9,38 @@ export default function OrderList() {
     return "No orders found.";
   }
 
-  const output = orders.map(order => {
-    const cart = Object.keys(order.cart).map(productId => {
-      const product = products.find(product => product.id === productId);
+  const output = orders.map((order) => {
+    const cart = Object.keys(order.cart).map((productId) => {
+      const product = products.find((product) => product.id === productId);
       if (!product) {
         return "Product not found.";
       }
 
       return (
         <div>
-          {product.name}: {order.cart[product.id]} X {product.price} som = {order.cart[product.id] * product.price} som
+          {product.name}: {order.cart[product.id]} X {product.price} som ={" "}
+          {order.cart[product.id] * product.price} som
         </div>
       );
     });
 
     return (
       <div className="Order">
-        <div><strong>Name</strong>: {order.name}</div>
-        <div><strong>Phone</strong>: {order.phone}</div>
-        <div><strong>Address</strong>: {order.address}</div>
-        <div><strong>Cart</strong>: {cart}</div>
+        <div>
+          <strong>Name</strong>: {order.name}
+        </div>
+        <div>
+          <strong>Phone</strong>: {order.phone}
+        </div>
+        <div>
+          <strong>Address</strong>: {order.address}
+        </div>
+        <div>
+          <strong>Cart</strong>: {cart}
+        </div>
       </div>
     );
   });
 
-  return (
-    <div className="OrderList">
-      {output}
-    </div>
-  );
+  return <div className="OrderList">{output}</div>;
 }
