4fd2e76d982afcbea232b8fb8751915174bf98e2
Seksenova Begaiym <begaym01@gmail.com>
2023-05-26 15:16:09 +0600

    formatted the document

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -8,20 +8,16 @@ import DeleteCategory from "../DeleteCategory/DeleteCategory";
 export default function CategoryList() {
   const { categories } = useContext(AppContext);
 
-  const output = categories.map(category => (
+  const output = categories.map((category) => (
     <li key={category.id}>
-      <NavLink to={'/category/' + category.path}>
-        {category.name}
-      </NavLink>
+      <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
       <DeleteCategory category={category} />
     </li>
   ));
 
   return (
     <div className="CategoryList">
-      <ul>
-        {output}
-      </ul>
+      <ul>{output}</ul>
       <AddCategory />
     </div>
   );
